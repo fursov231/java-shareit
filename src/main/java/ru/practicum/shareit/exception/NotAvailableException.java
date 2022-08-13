b7463eb8d82fb6fa40e199e1a7dd7df7187d7e9c
Vitaly <fursov231@hotmail.com>
2022-08-13 20:33:18 +0300

    style: fix code style

--- src/main/java/ru/practicum/shareit/exception/NotAvailableException.java
+++ src/main/java/ru/practicum/shareit/exception/NotAvailableException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class NotAvailableException extends RuntimeException{
+public class NotAvailableException extends RuntimeException {
     public NotAvailableException(final String message) {
         super(message);
     }
