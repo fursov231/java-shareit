0382293447f6206a57b8cce16df243fe91d96120
Vitaly <fursov231@hotmail.com>
2022-08-15 13:07:26 +0300

    fix: remove unused class and methods

--- src/main/java/ru/practicum/shareit/booking/util/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/util/BookingMapper.java
@@ -2,30 +2,9 @@ package ru.practicum.shareit.booking.util;
 
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingDtoWithBookerId;
-import ru.practicum.shareit.booking.dto.BookingDtoWithTime;
 import ru.practicum.shareit.booking.model.Booking;
 
 public class BookingMapper {
-    public static BookingDtoWithTime toBookingDto(Booking booking, Booking lastBooking, Booking nextBooking) {
-        return new BookingDtoWithTime(
-                booking.getId(),
-                booking.getStatus(),
-                booking.getBooker(),
-                booking.getItem(),
-                lastBooking,
-                nextBooking
-        );
-    }
-
-    public static Booking toBooking(BookingDtoWithTime bookingDtoWithTime) {
-        return Booking.builder()
-                .id(bookingDtoWithTime.getId())
-                .status(bookingDtoWithTime.getStatus())
-                .item(bookingDtoWithTime.getItem())
-                .booker(bookingDtoWithTime.getBooker())
-                .build();
-    }
-
     public static Booking dtoToBooking(BookingDto bookingDto) {
         return Booking.builder()
                 .start(bookingDto.getStart())
